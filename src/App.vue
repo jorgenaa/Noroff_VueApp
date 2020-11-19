3f630191c17d281eb4bcdc7682506da600b4ac94
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-11-19 02:29:39 +0100

    customized the folder structure & fetched the data with a created lifecycle hook

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+	
+	</div>
+</template>
+
+<script>
+
+
+export default {
+	name: 'App',
+	
+	created() {
+		const url = 'http://www.recipepuppy.com/api/';
+
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				this.recipes = data.results;
+			})
+			.catch(err => console.log(err));
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	
+}
+
+
+</style>
