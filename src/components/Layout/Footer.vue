66afa29146a7a83f9873f37efa7aab725710c633
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-11-19 16:03:38 +0100

    created components layout for Header and Footer

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+          
+    </footer> 
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+     
+}
+</script>
+
+<style scoped>
+    footer {
+        height: 150px;
+        background-color: #333;
+    }
+  
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Noroff VueApp</h1>    
+    </header> 
+</template>
+
+<script>
+export default {
+    name: 'Header',
+     
+}
+</script>
+
+<style scoped>
+    header {
+        height: 100px;
+        background-image: linear-gradient(to top, #0ba360 0%, #3cba92 100%);
+    }
+    h1 {
+        font-size: 48px;
+        color: purple;
+        text-align: center;
+        line-height: 100px;
+    }
+</style>
