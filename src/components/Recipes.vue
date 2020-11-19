a5120b788a3915b9f2467f98390bc0b869f9cfe7
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2020-11-19 15:02:01 +0100

    created a dumb component called Recipes that render the props from the App component

---
+++ src/components/Recipes.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="container" >
+		<div class="recipe-card" v-for="recipe in recipes" :key="recipe.title">
+			<a :href="recipe.href"><h3>{{ recipe.title }}</h3></a>
+			<img :src="recipe.thumbnail" />
+			<p><span>Ingredients:</span>{{ recipe.ingredients }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Recipes',
+    props: ['recipes']
+   
+};
+</script>
+
+<style scoped>
+.container {
+    margin: 60px auto;
+    max-width: 1200px; 
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.recipe-card {
+	flex-basis: 300px;
+	margin: 16px;
+	padding: 16px;
+	border: 1px solid black;
+  border-radius: 5px;
+}
+h3 {
+  font-size: 22px;
+}
+a {
+  text-decoration: none;
+  color: green;
+}
+p {
+  font-size: 16px;
+  letter-spacing: 0.5;
+  line-height: 1.5;
+}
+
+span {
+	font-weight: bold;
+	padding-right: 10px;
+}
+
+img {
+	width: 100%;
+  
+}
+</style>
